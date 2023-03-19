b61c743effb7d8982aafe720ea9b5831265375e8
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2023-03-20 01:30:24 +0530

    Error page is added

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import "./pageNotFound.scss";
-const pageNotFound = () => {
-  return (
-    <div>pageNotFound</div>
-  )
-}
+import React from "react";
 
-export default pageNotFound
+import "./PageNotFound.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
